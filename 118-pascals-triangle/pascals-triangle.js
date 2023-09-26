02937cce26b0d74fe0960252776f645aaa84743a
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2023-09-26 09:58:18 +0530

    Time: 57 ms (30.50%) | Memory: 42.3 MB (50.22%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    if(numRows==0) return
+    else if(numRows==1) return [[1]]
+    else if(numRows==2) return [[1],[1,1]]
+    else{
+        let arr = [[1],[1,1]]
+        let l
+        for(let i=3;i<=numRows;i++){
+            l = arr[i-2].length
+            let j=0
+            let temp = []
+            while(j<l){
+                if(j==0){
+                    temp.push(arr[i-2][j])
+                    temp.push(arr[i-2][j]+arr[i-2][j+1])
+                } else if (j==(l-1)){
+                    temp.push(arr[i-2][j])
+                }
+                else{
+                    temp.push(arr[i-2][j]+arr[i-2][j+1])
+                }
+                j++
+            }
+            arr.push(temp)
+        }
+        return arr
+    }
+    
+};
