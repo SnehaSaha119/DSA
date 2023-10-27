bb4cc1fcdbe615c1d313e5d77c4ec92e6cacfb1b
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2023-10-27 23:47:26 +0530

    Time: 57 ms (48.75%) | Memory: 42.7 MB (83.04%) - LeetSync

--- 62-unique-paths/unique-paths.ts
+++ 62-unique-paths/unique-paths.ts
@@ -20,8 +20,17 @@ function paths(m,i,n,j,arr){
     // if(i>=m || j>=n) return 0
     // else return paths(m,i+1,n,j) + paths(m,i,n,j+1)
 
-    if(i==m-1 && j==n-1) return 1
-    if(i>=m || j>=n) return 0
-    if(arr[i][j]!=-1) return arr[i][j]
-    else return arr[i][j] = paths(m,i+1,n,j,arr) + paths(m,i,n,j+1,arr)
+    // if(i==m-1 && j==n-1) return 1
+    // if(i>=m || j>=n) return 0
+    // if(arr[i][j]!=-1) return arr[i][j]
+    // else return arr[i][j] = paths(m,i+1,n,j,arr) + paths(m,i,n,j+1,arr)
+
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(i==0 || j==0) arr[i][j] = 1
+            else arr[i][j] = arr[i][j-1]+arr[i-1][j]
+        }
+    }
+
+    return arr[m-1][n-1]
 }
