1714875c5fa5dc51555a169a922f5d5f49cfcc25
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2023-11-07 09:51:31 +0530

    Time: 57 ms (48.57%) | Memory: 44.5 MB (25.32%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -0,0 +1,32 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    
+    for(let i=0;i<matrix.length;i++){
+        let arr = matrix[i]
+
+        let result = binary(arr,target)
+        if(result==true) return result
+    }
+
+    return false
+    
+};
+
+function binary(arr:number[],target){
+    let low=0
+    let high=arr.length-1
+    let mid;
+
+    while(low<=high){
+        mid=Math.floor((low+high)/2)
+        //console.log(low,high,mid)
+
+        if(arr[mid]==target) return true
+        else if(arr[mid]<target) low=mid+1
+        else high=mid-1
+
+        //console.log(low,high,mid)
+
+    }
+
+    return false
+}
