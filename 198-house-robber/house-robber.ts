b2cbc84c77cdb2fa7711086167a95c5707ff687e
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2023-11-21 00:46:32 +0530

    Time: 63 ms (14.76%) | Memory: 43.3 MB (22.38%) - LeetSync

--- 198-house-robber/house-robber.ts
+++ 198-house-robber/house-robber.ts
@@ -1,20 +1,36 @@
 function rob(nums: number[]): number {
+
+    let value = new Array(nums.length).fill(-1)
+    let result = allSubSequence(nums.length-1,nums,value)
+    return result
     
-    let oddMax=0;
-    let evenMax=0;
+    // let oddMax=0;
+    // let evenMax=0;
         
-    for(let i=0;i<nums.length;i=i+1) {
-        if (i%2==0) {
-            evenMax= evenMax+nums[i];
-            evenMax= Math.max(evenMax,oddMax); 
-        } else {
-            oddMax= oddMax+nums[i];
-        oddMax= Math.max(oddMax,evenMax);    
-        }
+    // for(let i=0;i<nums.length;i=i+1) {
+    //     if (i%2==0) {
+    //         evenMax= evenMax+nums[i];
+    //         evenMax= Math.max(evenMax,oddMax); 
+    //     } else {
+    //         oddMax= oddMax+nums[i];
+    //     oddMax= Math.max(oddMax,evenMax);    
+    //     }
         
-    }
+    // }
         
-    return Math.max(oddMax,evenMax);
+    // return Math.max(oddMax,evenMax);
 
     
 };
+
+function allSubSequence(index:number,nums:number[],value:number[]){
+    if(index==0) return nums[index]
+    if(index<0) return 0
+
+    if(value[index]!=-1) return value[index]
+
+    let pick = nums[index] + allSubSequence((index-2),nums,value)
+    let notPick = 0 + allSubSequence((index-1),nums,value)
+
+    return value[index] = Math.max(pick,notPick)
+}
