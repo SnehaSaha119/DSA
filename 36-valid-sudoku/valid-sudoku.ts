7e6f1e623f340e0c2f84682b2e805bf3a8656857
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2023-12-04 10:21:07 +0530

    Time: 69 ms (66.61%) | Memory: 45.3 MB (88.69%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.ts
@@ -0,0 +1,68 @@
+function isValidSudoku(board: string[][]): boolean {
+    let l=board.length
+    let flag = true
+    
+    for(let i=0;i<l;i++){
+        
+        let ro;
+        let col;
+
+        //row
+        let row = new Set()
+        for(let j=0;j<l;j++){
+            if (board[i][j] !== ".") {
+                if(row.has(board[i][j])){
+                    flag = false
+                    break
+                }
+                row.add(board[i][j])
+            }
+        }
+
+        //column
+        let column = new Set()
+        for(let k=0;k<l;k++){
+            if (board[k][i] !== ".") {
+                if(column.has(board[k][i])){ //set,map has O(1)
+                    flag = false
+                    break
+                }
+                column.add(board[k][i])
+            }
+        }
+
+        if(flag==false) break
+        
+    }
+
+    //box
+        for (let i = 0; i < l; i++) {
+            const set = new Set<string>();
+            const rowStart = Math.floor(i / 3) * 3;
+            const colStart = (i % 3) * 3;
+            for (let j = 0; j < 9; j++) {
+                const row = rowStart + Math.floor(j / 3);
+                const col = colStart + (j % 3);
+                const cell = board[row][col];
+                if (cell !== ".") {
+                    if (set.has(cell)) {
+                        flag = false
+                        break
+                    } else {
+                        set.add(cell);
+                    }
+            }
+        }
+    }
+
+
+    return flag
+
+
+
+    /*
+if(!set.add(Character.toString(board[i][j])+"Row Wise"+i) || !set.add(Character.toString(board[i][j])+"Column Wise"+j) || !set.add(Character.toString(board[i][j])+"Sub Grid"+i/3+"And"+j/3)) { 
+    */
+
+    
+};
