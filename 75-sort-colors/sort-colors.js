3cb88543d222f5119b962930d61110c64da12d50
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2024-03-26 22:44:55 +0530

    Time: 46 ms (89.45%) | Memory: 48.9 MB (54.44%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,36 +3,60 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let count0 = 0
-    let count1 = 0
-    let count2 = 0
+    //Better approach O(N) + O(N)
+    // let count0 = 0
+    // let count1 = 0
+    // let count2 = 0
 
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]==0) count0++
-        else if(nums[i]==1) count1++
-        else if(nums[i]==2) count2++
+    // for(let i=0;i<nums.length;i++){
+    //     if(nums[i]==0) count0++
+    //     else if(nums[i]==1) count1++
+    //     else if(nums[i]==2) count2++
 
+    // }
+
+    // let k=0
+
+    // while(count0!=0){
+    //     nums[k]=0
+    //     k++
+    //     count0--
+    // }
+
+    // while(count1!=0){
+    //     nums[k]=1
+    //     k++
+    //     count1--
+    // }
+
+    // while(count2!=0){
+    //     nums[k]=2
+    //     k++
+    //     count2--
+    // }
+
+    // return nums
+
+    //Optimized O(N)
+
+    let low = 0
+    let high = nums.length-1
+    let mid = 0
+
+    while(mid<=high){
+        if(nums[mid]==0){
+            let temp = nums[mid]
+            nums[mid] = nums[low]
+            nums[low] = temp
+            low++
+            mid++
+        }else if(nums[mid]==1){
+            mid++
+        }else if(nums[mid]==2){
+            let temp = nums[mid]
+            nums[mid] = nums[high]
+            nums[high] = temp
+            high--
+        }
     }
-
-    let k=0
-
-    while(count0!=0){
-        nums[k]=0
-        k++
-        count0--
-    }
-
-    while(count1!=0){
-        nums[k]=1
-        k++
-        count1--
-    }
-
-    while(count2!=0){
-        nums[k]=2
-        k++
-        count2--
-    }
-
-    return nums
 };
