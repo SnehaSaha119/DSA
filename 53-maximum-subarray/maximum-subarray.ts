ae7b635ebebf20c0b299e6cfd4f6b873a5876f3b
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2024-07-06 11:00:32 +0530

    Time: 92 ms (9.95%) | Memory: 59.8 MB (41.77%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.ts
+++ 53-maximum-subarray/maximum-subarray.ts
@@ -11,20 +11,34 @@ function maxSubArray(nums: number[]): number {
 
     // return max
 
-    //-----optimized-------
+    // //-----optimized-------
    
-    let sum=0
-    let max = Number.MIN_SAFE_INTEGER
+    // let sum=0
+    // let max = Number.MIN_SAFE_INTEGER
+    // for(let i=0;i<nums.length;i++){
+    //     sum=sum+nums[i]
+    //     if(sum>max) max=sum
+    //     if(sum<0) 
+    //     {
+    //         sum=0
+    //         //console.log("88888")
+    //     }
+
+    //     console.log(sum)
+    // }
+
+    // return max
+
+    let max = Number.MIN_SAFE_INTEGER;
+    let sum = 0;
+
     for(let i=0;i<nums.length;i++){
-        sum=sum+nums[i]
-        if(sum>max) max=sum
-        if(sum<0) 
-        {
-            sum=0
-            //console.log("88888")
+        sum = sum + nums[i]
+        max = Math.max(sum,max)
+        if(sum<0){
+            sum = 0
         }
 
-        console.log(sum)
     }
 
     return max
