1ef253384727fb05b1a46e7dba3c51458b06852d
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2024-07-09 00:08:24 +0530

    Time: 59 ms (76.94%) | Memory: 51.9 MB (93.13%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.ts
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.ts
@@ -1,11 +1,9 @@
 function runningSum(nums: number[]): number[] {
 
-    let prefixSum = [nums[0]];
-
     for(let i=1;i<nums.length;i++){
-        prefixSum[i] = prefixSum[i-1] + nums[i];
+        nums[i] = nums[i-1] + nums[i];
     }
 
-    return prefixSum
+    return nums
 
 };
