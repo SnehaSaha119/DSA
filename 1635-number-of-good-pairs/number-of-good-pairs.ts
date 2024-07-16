8a36cb20bc7bdc4b523a2e970f5742a0e2489225
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2024-07-16 22:16:57 +0530

    Time: 66 ms (15.68%) | Memory: 50 MB (70.81%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.ts
+++ 1635-number-of-good-pairs/number-of-good-pairs.ts
@@ -1,11 +1,27 @@
 function numIdenticalPairs(nums: number[]): number {
+    //-----brute
+    // let n=nums.length;
+    // let count=0;
 
+    // for(let i=0;i<n-1;i++){
+    //     for(let j=0;j<n;j++){
+    //         if(i<j && nums[i]==nums[j]) count++;
+    //     }
+    // }
+
+    // return count;
+
+    //---better
     let n=nums.length;
     let count=0;
+    let map = {};
 
-    for(let i=0;i<n-1;i++){
-        for(let j=0;j<n;j++){
-            if(i<j && nums[i]==nums[j]) count++;
+    for(let i=0;i<n;i++){
+        if(map[nums[i]]){
+            count = count + map[nums[i]];
+            map[nums[i]]++;
+        }else{
+            map[nums[i]] = 1;
         }
     }
 
