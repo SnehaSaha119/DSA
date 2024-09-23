1039949aa252fd2699a0209700d926db713f85e2
SnehaSaha119 <60418301+SnehaSaha119@users.noreply.github.com>
2024-09-23 22:10:12 +0530

    Time: 72 ms (32.66%) | Memory: 53.1 MB (15.33%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -11,70 +11,106 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    let head1=list1
-    let head2=list2
-    let head3=null;
-    let p=head3
-    while(head1!=null && head2!=null){
-        console.log(head3)
-        if(head1.val<=head2.val){
-            if(head3==null){
-                head3=head1
-                head1=head1.next
-                head3.next=null
-                p=head3
-            }else{
-            p.next=head1
-            head1=head1.next
-            p=p.next
-            p.next=null
-            }
-        }else if(head2.val<head1.val){
-            if(head3==null){
-                head3=head2
-                head2=head2.next
-                head3.next=null
-                p=head3
-            }else{
+    // let head1=list1
+    // let head2=list2
+    // let head3=null;
+    // let p=head3
+    // while(head1!=null && head2!=null){
+    //     console.log(head3)
+    //     if(head1.val<=head2.val){
+    //         if(head3==null){
+    //             head3=head1
+    //             head1=head1.next
+    //             head3.next=null
+    //             p=head3
+    //         }else{
+    //         p.next=head1
+    //         head1=head1.next
+    //         p=p.next
+    //         p.next=null
+    //         }
+    //     }else if(head2.val<head1.val){
+    //         if(head3==null){
+    //             head3=head2
+    //             head2=head2.next
+    //             head3.next=null
+    //             p=head3
+    //         }else{
             
-            p.next=head2
-            head2=head2.next
-            p=p.next
-            p.next=null
-            }
-        }
-    }
+    //         p.next=head2
+    //         head2=head2.next
+    //         p=p.next
+    //         p.next=null
+    //         }
+    //     }
+    // }
 
-    while(head1!=null){
-        if(head3==null){
-                head3=head1
-                head1=head1.next
-                head3.next=null
-                p=head3
-        }else{
+    // while(head1!=null){
+    //     if(head3==null){
+    //             head3=head1
+    //             head1=head1.next
+    //             head3.next=null
+    //             p=head3
+    //     }else{
 
-        p.next=head1
-        head1=head1.next
-        p=p.next
-        p.next=null
-        }
-    }
+    //     p.next=head1
+    //     head1=head1.next
+    //     p=p.next
+    //     p.next=null
+    //     }
+    // }
 
-    while(head2!=null){
-        if(head3==null){
-                head3=head2
-                head2=head2.next
-                head3.next=null
-                p=head3
-            }else{
+    // while(head2!=null){
+    //     if(head3==null){
+    //             head3=head2
+    //             head2=head2.next
+    //             head3.next=null
+    //             p=head3
+    //         }else{
             
-            p.next=head2
-            head2=head2.next
-            p=p.next
-            p.next=null
-            }
-    }
+    //         p.next=head2
+    //         head2=head2.next
+    //         p=p.next
+    //         p.next=null
+    //         }
+    // }
 
-    return head3
+    // return head3
+
+     if (!list1) return list2;
+        if (!list2) return list1;
+
+        let head: ListNode | null = null;
+        let cur: ListNode | null = null;
+
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+        
+        cur = head;
+
+        while (list1 !== null && list2 !== null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+
+        // Attach the remaining elements
+        if (list1 !== null) {
+            cur.next = list1;
+        } else {
+            cur.next = list2;
+        }
+
+        return head;
 
 };
